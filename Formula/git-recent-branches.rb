18f2d6ff3f91e32e680f70dd23e4d4be4d584b0a
Andrew Marshall <andrew@johnandrewmarshall.com>
2017-01-17 11:50:56 -0500

    git-recent-branches: v0.1.0

--- Formula/git-recent-branches.rb
+++ Formula/git-recent-branches.rb
@@ -1,6 +1,8 @@
 class GitRecentBranches < Formula
   desc "Git command plugin to list recently checked-out branches in the current repository."
   homepage "https://github.com/amarshall/git-recent-branches"
+  url "https://github.com/amarshall/git-recent-branches/archive/v0.1.0.tar.gz"
+  sha256 "261735f6d7be0ea11250d08e7250bdb084f9e819ca47efb1ee62e7e31a0f79e9"
   head "https://github.com/amarshall/git-recent-branches.git"
 
   def install
